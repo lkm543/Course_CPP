37a7b7cf5d6516a63b591f8352d6a7a798233cee
lkm543 <lkm543@gmail.com>
2019-08-10 19:51:02 +0800

    Change course time

--- Chapter00.php
+++ Chapter00.php
@@ -2,7 +2,7 @@
 
 
 <h2>大綱</h2>
-<p>歡迎大家來到資工訓練班學習C/C++課程，這一章節會先簡介一些基本的計算機概論，像是電腦的運作原理、程式語言的意涵、有哪些程式語言、C/C++的特色與強項等，最後附上本期的課程大綱，唯速度會再因應上課情形有所調整。<br>(本章預計授課時間：25-35 Mins)</p>
+<p>歡迎大家來到資工訓練班學習C/C++課程，這一章節會先簡介一些基本的計算機概論，像是電腦的運作原理、程式語言的意涵、有哪些程式語言、C/C++的特色與強項等，最後附上本期的課程大綱，唯速度會再因應上課情形有所調整。<br>(本章預計授課時間：55-65 Mins)</p>
 
 <h2>課程介紹</h2>
 <iframe width="640" height="320" src="https://www.youtube.com/embed/kVFi1rNNv2o" frameborder="0" allowfullscreen></iframe>
@@ -16,38 +16,6 @@
 	<li>自行學習其他語言時能夠較快上手</li>
 	<li>能夠自行學習C/C++較為進階的使用</li>
 	<li>度過剛開始學習程式時的撞牆期</li>
-</ul>
-
-<h2>進度規劃</h2>
-<ol>
-	<li>Chapter 00 C/C++程式語言簡介</li>
-	<li>Chapter 01 基本輸入輸出</li>
-	<li>Chapter 02 資料型態與變數</li>
-	<li>Chapter 03 控制標準輸出</li>
-	<li>Chapter 04 條件判斷</li>
-	<li>Chapter 05 迴圈</li>
-	<li>Homework 1</li>
-	<li>Chapter 06 陣列 字元與字串</li>
-	<li>Chapter 07 指標(Pointer)</li>
-	<li>Chapter 08 函式(Function)、遞迴</li>
-	<li>Homework 2</li>
-	<li>Chapter 09 函式庫的使用</li>
-	<li>Chapter 10 檔案處理(File)</li>
-	<li>Homework 3</li>
-	<li>Chapter 11 結構(Structure)</li>
-	<li>Chapter 12 物件的使用、重載運算子</li>
-	<li>Chapter 13 類別的關係、繼承</li>
-	<li>Chapter 14 類別的進階使用</li>
-	<li>Homework 4</li>
-	<li>Chapter 15 Template</li>
-	<li>Chapter 16 期末考/Demo</li>
-</ol>
-<ul>
-	<li>1~7項(至作業1為止)是前半段的基本語法，學習完基本語法後應該可以輕易地將問題的思考流程轉化成程式的語法。</li>
-	<li>8-11項(含作業2)會慢慢導入函式的概念，使得程式的可讀性、可維護性增加，也會介紹指標的概念。</li>
-	<li>12-14(含作業3)為外部函式庫的導入與檔案讀寫的應用，學到這裡應該可以自行寫出一隻完整可以用來解決生活上問題的小程式。</li>
-	<li>15-19項(含作業4)是物件化導向的應用，在程式界所有的東西為了合作開發與維護方便，都會寫成物件化的形式來進行，因此最後我們將練習物件化的使用與撰寫。</li>
-	<li>20項的Template為補充用，視課程進度而定不一定會上到，因此不列入期末考試的範圍。</li>
 </ul>
 
 <h2>電腦運作基本原理</h2>
