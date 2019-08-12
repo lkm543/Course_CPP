61b5a88e0eeb4b94be05e446e07ff78bfef9ddc7
lkm543 <lkm543@gmail.com>
2019-08-12 11:21:45 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -6,31 +6,34 @@
     <h2 class="star">課程目錄</h2>
     <ul class="sb_menu etabs">
       
-          <li class='tab' data-tabid="Chapter00"><a href="#Chapter00">Chapter 00 C/C++程式語言簡介</a></li>
+          <li class='tab' data-tabid="Chapter00"><a href="#Chapter00">Chapter 00 電腦運作原理與C/C++簡介</a></li>
           <li class='tab' data-tabid="Chapter01"><a href="#Chapter01">Chapter 01 基本輸入輸出</a></li>
           <li class='tab' data-tabid="Chapter02"><a href="#Chapter02">Chapter 02 資料型態與變數</a></li>
           <li class='tab' data-tabid="Chapter03"><a href="#Chapter03">Chapter 03 控制標準輸出</a></li>
           <li class='tab' data-tabid="HW1"><a href="#HW1">Homework 1</a></li>
-          <li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Chapter 04 條件判斷、例外判別</a></li>
+          <li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Chapter 04 條件判斷例外判斷</a></li>
+          <li class='tab' data-tabid="Chapter05"><a href="#Chapter05">Chapter 05 例外判斷</a></li>
           <li class='tab' data-tabid="HW2"><a href="#HW2">Homework 2</a></li>
-          <li class='tab' data-tabid="Chapter05"><a href="#Chapter05">Chapter 05 迴圈</a></li>
+          <li class='tab' data-tabid="Chapter06"><a href="#Chapter06">Chapter 06 迴圈</a></li>
           <li class='tab' data-tabid="HW3"><a href="#HW3">Homework 3</a></li>
-          <li class='tab' data-tabid="Chapter06"><a href="#Chapter06">Chapter 06 陣列 字元與字串</a></li>
+          <li class='tab' data-tabid="Chapter07"><a href="#Chapter07">Chapter 07 陣列</a></li>
+          <li class='tab' data-tabid="Chapter08"><a href="#Chapter08">Chapter 08 字元與字串</a></li>
           <li class='tab' data-tabid="HW4"><a href="#HW1">Homework 4</a></li>
-          <li class='tab' data-tabid="Chapter07"><a href="#Chapter07">Chapter 07 指標(Pointer)</a></li>
+          <li class='tab' data-tabid="Chapter09"><a href="#Chapter09">Chapter 09 指標(Pointer)</a></li>
           <li class='tab' data-tabid="HW5"><a href="#HW1">Homework 5</a></li>
-          <li class='tab' data-tabid="Chapter08"><a href="#Chapter08">Chapter 08 函式(Function)、遞迴</a></li>
+          <li class='tab' data-tabid="Chapter10"><a href="#Chapter10">Chapter 10 函式(Function)與遞迴</a></li>
           <li class='tab' data-tabid="HW6"><a href="#HW2">Homework 6</a></li>
-          <li class='tab' data-tabid="Chapter09"><a href="#Chapter09">Chapter 09 函式庫的使用</a></li>
-          <li class='tab' data-tabid="Chapter10"><a href="#Chapter10">Chapter 10 檔案處理(File)</a></li>
+          <li class='tab' data-tabid="Chapter11"><a href="#Chapter11">Chapter 11 函式庫的使用</a></li>
+          <li class='tab' data-tabid="Chapter12"><a href="#Chapter12">Chapter 12 檔案處理(File)</a></li>
           <li class='tab' data-tabid="HW7"><a href="#HW3">Homework 7</a></li>
-          <li class='tab' data-tabid="Chapter11"><a href="#Chapter11">Chapter 11 結構(Structure)</a></li>
-          <li class='tab' data-tabid="Chapter12"><a href="#Chapter12">Chapter 12 物件的使用、重載運算子</a></li>
-          <li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Chapter 13 類別的關係、繼承</a></li>
-          <li class='tab' data-tabid="Chapter14"><a href="#Chapter14">Chapter 14 類別的進階使用</a></li>
+          <li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Chapter 13 標頭檔的建立</a></li>
+          <li class='tab' data-tabid="Chapter14"><a href="#Chapter14">Chapter 14 結構(Structure)</a></li>
           <li class='tab' data-tabid="HW8"><a href="#HW4">Homework 8</a></li>
-          <li class='tab' data-tabid="Chapter15"><a href="#Chapter15">Chapter 15 Template</a></li>
-          <li class='tab' data-tabid="Chapter16"><a href="#Chapter16">Chapter 16 期末考/Demo</a></li>
+          <li class='tab' data-tabid="Chapter15"><a href="#Chapter15">Chapter 15 物件的使用、重載運算子</a></li>
+          <li class='tab' data-tabid="Chapter16"><a href="#Chapter16">Chapter 16 類別的關係、繼承</a></li>
+          <li class='tab' data-tabid="Chapter17"><a href="#Chapter17">Chapter 17 類別的進階使用</a></li>
+          <li class='tab' data-tabid="Chapter18"><a href="#Chapter18">Chapter 18 Template</a></li>
+          <li class='tab' data-tabid="Chapter19"><a href="#Chapter19">Chapter 19 期末考/Demo</a></li>
     </ul>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
