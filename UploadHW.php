c311326e4e746b6a76321053fba816b65c7e87ed
lkm543 <lkm543@gmail.com>
2019-08-20 13:52:08 +0800

    ip change

--- UploadHW.php
+++ UploadHW.php
@@ -8,13 +8,26 @@ if ($_FILES["fileToUpload"]["error"] > 0){
 	$Name=$_POST['name'];
 	$Comment=$_POST['comment'];
 	$UploadDate=date('Y-m-d H:i:s');
-	if (!empty($_SERVER["HTTP_CLIENT_IP"])){
-	    $ip = $_SERVER["HTTP_CLIENT_IP"];
-	}elseif(!empty($_SERVER["HTTP_X_FORWARDED_FOR"])){
-	    $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
-	}else{
-	    $ip = $_SERVER["REMOTE_ADDR"];
-	}
+	
+    function get_client_ip() {
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+           $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
+    }
+    $ip = get_client_ip();
 	     
     $_SESSION['Filename']=$_FILES["fileToUpload"]["name"];
 	$_SESSION['FileType']=$_FILES["fileToUpload"]["type"];
