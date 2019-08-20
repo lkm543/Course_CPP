930d1c6808da456cbd584876318fbe1b8eeb5980
lkm543 <lkm543@gmail.com>
2019-08-20 11:45:49 +0800

    Homework template

--- HW8.php
+++ HW8.php
@@ -1,9 +1,24 @@
-<h2>4-1：資料庫查找系統</h2>
+<h1>作業8 列舉、結構與標頭檔</h1>
+
+<h2>8-1：列舉</h2>
+	<h3></h3>
+	<h3></h3>
+	<img src="images/HW8_1.png" style="display:block; margin:auto;">
+	
+<h2>8-2：結構─資料庫查找系統</h2>
 	<h3>下面是Ch11 Mission8的程式碼，請將其修改後，讓該程式具有可以藉由輸入latitude來找出該筆資料的功能。</h3>
 	<h3>(資料下載：<a href="http://ppt.cc/qKVEE">http://ppt.cc/qKVEE</a>)</h3>
-	<script src="https://gist.github.com/anonymous/a473abf2cdaf64364ff241764e1f49e9.js"></script>
-	<img src="images/HW3_3_1.png" style="display:block; margin:auto;">
-	<h3>Hint：在C++中比較兩字串是否相同，可用s1.compare(s2)==0判別</h3>
+	<img src="images/HW8_2.png" style="display:block; margin:auto;">
+
+<h2>8-3：標頭檔的使用</h2>
+	<h3></h3>
+	<h3></h3>
+	<img src="images/HW8_3.png" style="display:block; margin:auto;">
+
+<h2>8-4：自行寫一個標頭檔</h2>
+	<h3></h3>
+	<h3></h3>
+	<img src="images/HW8_4.png" style="display:block; margin:auto;">
 
 <br>
 <br>
