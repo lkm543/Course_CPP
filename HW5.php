f8c83bc386d6cc891ba22584d1689d204224e347
lkm543 <lkm543@gmail.com>
2019-08-23 23:21:20 +0800

    HW5 Update

--- HW5.php
+++ HW5.php
@@ -17,13 +17,6 @@
 	<h3>讓使用者輸入一個正整數N，而後使用malloc開出一個長度為N的整數陣列，讓使用者可以依序輸入陣列中每一個元素的值，並輸出其平均數。</h3>
 	<img src="images/HW5_2.png" style="display:block; margin:auto;">
 
-<h2>5-3：速度比較─透過指標操控陣列</h2>
-	<h3>Notice：速度會根據Compiler而定，您的結果不一定會如同下圖所示</h3>
-
-<h2>5-4：使用指標控制字串</h2>
-	<h3></h3>
-	<img src="images/HW5_4.png" style="display:block; margin:auto;">
-
 <br>
 <br>
 <br>
