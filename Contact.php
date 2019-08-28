5e972a6b31fa4b646eb8786241001a194764d8fd
lkm543 <lkm543@gmail.com>
2019-08-28 11:39:55 +0800

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -6,5 +6,5 @@
 <p>Linkedin: <a href="https://www.linkedin.com/in/lkm543/">https://www.linkedin.com/in/lkm543/</a></p>
 <p>Github: <a href="https://github.com/lkm543">https://github.com/lkm543</a></p>
 <p>Website: <a href="https://www.lkm543.site/">https://www.lkm543.site/</a></p>
-<p>Website: <a href="https://lkm543.site/Blog/">https://lkm543.site/Blog/</a></p>
+<p>Blog: <a href="https://lkm543.site/Blog/">https://lkm543.site/Blog/</a></p>
 <p>Source Code: <a href="https://github.com/lkm543/Course_CPP">https://github.com/lkm543/Course_CPP</a></p>
