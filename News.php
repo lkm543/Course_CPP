0208d40ad9d075ee8ff0436a0d26940908bbe5b1
lkm543 <lkm543@gmail.com>
2019-09-17 13:33:53 +0800

    Update News.php

--- News.php
+++ News.php
@@ -8,6 +8,11 @@ SSID：CSIE_guest<br>
 帳號：guest_H7C8B<br>
 密碼：IM6DMBRR</h2>
 
+<h2>2019/09/11</h2>
+<ul>
+	<li>319期期末考試已批改完畢，若無收到回信請告知</li>
+</ul>
+
 <h2>2019/09/02</h2>
 <ul>
 	<li>HW1~7參考答案已上傳<a href="/Course/Cpp/Download/">Download區</a></li>
