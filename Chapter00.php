4d8532938b89c6566376753f68f58256507682a7
lkm543 <lkm543@gmail.com>
2020-03-15 18:34:23 +0800

    新增影片連結

--- Chapter00.php
+++ Chapter00.php
@@ -4,25 +4,31 @@
 <p>歡迎大家來到資工訓練班學習C/C++課程，這一章節會先簡介一些基本的計算機概論，像是電腦的運作原理、程式語言的意涵、有哪些程式語言、C/C++的特色與強項等，最後附上本期的課程大綱，唯速度會再因應上課情形有所調整。<br>(本章預計授課時間：65-75 Mins)</p>
 
 <h2>課程介紹</h2>
-<iframe width="640" height="320" src="https://www.youtube.com/embed/kVFi1rNNv2o" frameborder="0" allowfullscreen></iframe>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/OoJaiztKBrk"></iframe>
+</div>
 
-<h2>課程目標</h2>
-<ul>
-	<li>知道程式背後運作的基本原理跟邏輯</li>
-	<li>能夠了解程式語言的基本語法</li>
-	<li>了解物件導向的基本概念與使用</li>
-	<li>會撰寫C/C++小程式</li>
-	<li>自行學習其他語言時能夠較快上手</li>
-	<li>能夠自行學習C/C++較為進階的使用</li>
-	<li>度過剛開始學習程式時的撞牆期</li>
-</ul>
+<h2>電腦組成與運作</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/E6OeiGzLjgg"></iframe>
+</div>
 
-<h2>電腦運作基本原理</h2>
-<iframe width="640" height="320" src="https://www.youtube.com/embed/3x2Ge6DOPe4" frameborder="0" allowfullscreen></iframe>
+<h2>二進位與十進位</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/_Pzx_n1_Cgs"></iframe>
+</div>
 
-<h2>程式語言簡介</h2>
-<iframe width="640" height="320" src="https://www.youtube.com/embed/_U2RxcDOIg4" frameborder="0" allowfullscreen></iframe>
+<h2>程式語言介紹</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/T7RRChx6VZE"></iframe>
+</div>
 
-<h2>開發工具介紹</h2>
-<iframe width="640" height="320" src="https://www.youtube.com/embed/bxWU7WDBraM" frameborder="0" allowfullscreen></iframe>
+<h2>C/C++簡介</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/agXBaiayvt4"></iframe>
+</div>
 
+<h2>開發環境介紹</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ooIpz_ovp9A"></iframe>
+</div>
--- Chapter10.php
+++ Chapter10.php
@@ -3,15 +3,62 @@
 <h2>大綱</h2>
 <p>函式的意思是把一段會重複用到的程式碼獨立寫出來，每當我們會需要使用的時候便直接呼叫該函式即可，可避免同樣的程式碼重複出現，同時增加程式碼的可讀性與可維護性。<br>(本章預計授課時間：140-160 Mins)</p> 
 
-<h2>函式介紹、函式─指標與陣列</h2>
-<video id="movie" preload="meta" poster="poster.png" width="640" height="360">
-  <source src=" Videos/7_1.mp4" type="video/mp4" />
-  您的瀏覽器不支援影片播放
-</video>
+<h2>函式概論</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/u81rCvZwZg8"></iframe>
+</div>
 
-<h2>函式─函式多載與遞迴</h2>
-<video id="movie" preload="meta" poster="poster.png" width="640" height="360">
-  <source src=" Videos/7_2.mp4" type="video/mp4" />
-  您的瀏覽器不支援影片播放
-</video>
+<h2>習題講解</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/w8tfwF8JCzw"></iframe>
+</div>
 
+<h2>變數類別</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/g1XEVnyw6lQ"></iframe>
+</div>
+
+<h2>傳值與傳參考</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/7KzO3UHINSU"></iframe>
+</div>
+
+<h2>習題講解</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/nBDimNVhMS0"></iframe>
+</div>
+
+<h2>傳遞陣列參數</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/rs8OpjcjoOk"></iframe>
+</div>
+
+<h2>習題講解</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/lkjiR6BJ5ac"></iframe>
+</div>
+
+<h2>函式宣告</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/iyrYD6VlWSE"></iframe>
+</div>
+
+<h2>習題講解</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/fgTzy_hk_q0"></iframe>
+</div>
+
+<h2>遞迴</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/mpcUEn4l5IQ"></iframe>
+</div>
+
+<h2>習題講解</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ECdmDlkqmCI"></iframe>
+</div>
+
+<h2>重點回顧</h2>
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/_ckiqpiUYLo"></iframe>
+</div>
